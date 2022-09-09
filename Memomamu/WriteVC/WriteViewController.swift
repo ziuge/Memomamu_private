801e8cfdec58c79ad07fd31f46be52cf2261e25f
ziuge <xwcjwwd@ewhain.net>
2022-09-09 21:06:58 +0900

    UI components

--- Memomamu/WriteVC/WriteViewController.swift
+++ Memomamu/WriteVC/WriteViewController.swift
@@ -13,6 +13,27 @@ class WriteViewController: UIViewController {
     
     let localRealm = try! Realm()
     
+    var viewButton: UIButton = {
+        let view = UIButton()
+        view.setImage(UIImage(named: "todayButton"), for: .normal)
+        view.tintColor = Constants.Color.text
+        return view
+    }()
+    
+    var dateLabel: UILabel = {
+        let view = UILabel()
+        view.textColor = Constants.Color.text
+        view.text = "17, April"
+        view.font = Constants.Font.head
+        return view
+    }()
+    
+    var paperView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
     lazy var tableView: UITableView = {
         let view = UITableView()
         view.backgroundColor = Constants.Color.paper
@@ -21,8 +42,6 @@ class WriteViewController: UIViewController {
         view.register(WriteTableViewCell.self, forCellReuseIdentifier: WriteTableViewCell.reuseIdentifier)
         return view
     }()
-    
-    
     
     var contents: Results<Content>! {
         didSet {
@@ -38,10 +57,16 @@ class WriteViewController: UIViewController {
     }
     
     func configure() {
-        
+        [viewButton].forEach {
+            view.addSubview($0)
+        }
     }
     
     func setConstraints() {
+        viewButton.snp.makeConstraints { make in
+            make.topMargin.equalTo(view).offset(62)
+            make.trailingMargin.equalTo(view).offset(30)
+        }
         
     }
     
