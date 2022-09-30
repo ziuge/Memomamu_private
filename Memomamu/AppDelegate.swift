e23134d0c3f4ad62a26204c2d1a5f4e3dff9467f
ziuge <xwcjwwd@ewhain.net>
2022-09-30 18:54:14 +0900

    Add LaunchScreen Delay

--- Memomamu/AppDelegate.swift
+++ Memomamu/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        Thread.sleep(forTimeInterval: 3.0)
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
